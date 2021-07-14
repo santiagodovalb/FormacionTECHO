0dca2efd3866159ade5b4670a006c2f5bd5790ef
Santiago Videla <santiagovidela@Santiagos-MBP.fibertel.com.ar>
2021-07-14 14:54:14 -0300

    boiler front

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Sidebar from './components/SideBar';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
