cb3cf455b0ba4eaf7aaa8175b6f21eb8c40e47f2
Santiago Videla <santiagovidela@Santiagos-MBP.fibertel.com.ar>
2021-07-16 17:30:01 -0300

    modularizando login

---
+++ src/containers/login/loginUser.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import "./index.css";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../redux/user";
+import { sectionStyle } from "./style";
+import Logo from "../../assets/logo.png";
+import { useHistory } from "react-router";
+import axios from 'axios'
+import { message } from 'antd'
+import 'antd/dist/antd.css';
+import validator from 'validator';
+
+
+
+const LoginUser = () => {
+
+    const  dispatch = useDispatch()
+    const history = useHistory()
+    const [form, setForm] = useState({});
+    const [other, setOther] = useState("");
+  
+    const onChange = (e) => {
+      const { target } = e;
+      setForm({ ...form, [target.name]: target.value });
+      
+    };
+    const isEmail = () => validator.isEmail(form.email)
+  
+    const onSubmit = (e) => {
+      e.preventDefault();
+      if(isEmail()) {
+      axios.post("/api/users/login", form)
+      .then(res => res.data)
+      .then(user => {
+        dispatch(setUser(user))
+        message.success('Logged in')
+        return user
+        })
+        .then((user) => history.push(`/user/${user.id}`))
+      .catch(err => {
+        message.error('Bad credentials')
+        return err
+      
+      })
+      
+    }};
+  
+    const onOtherSubmit = (e) => setOther(e.target.id);
+
+    
+
+    return (
+        <form className="pb-3" onChange={onChange} onSubmit={onSubmit}>
+          <div className="mb-3">
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              placeholder="Correo"
+              required="true"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              placeholder="Contraseña"
+              required="true"
+            />
+          </div>
+          <button type="submit" className="btn btn-secondary mt-3">
+            Ingresar
+          </button>
+          <div className="m-3" onClick={onOtherSubmit}>
+          </div>
+        </form>
+    )
+
+}
+
+export default LoginUser;
