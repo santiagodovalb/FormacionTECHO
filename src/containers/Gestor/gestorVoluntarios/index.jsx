354b2a470f04babb5257ae0420c8b45e4c97af56
LucasTaboada111 <lucasdanieltaboada@gmail.com>
2021-07-26 12:06:14 -0300

    vista voluntarios gestor done

--- src/containers/Gestor/gestorVoluntarios/index.jsx
+++ src/containers/Gestor/gestorVoluntarios/index.jsx
@@ -1,68 +1,103 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { useSelector, useDispatch} from 'react-redux'
-import { useHistory} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { Table, Button,Select } from "antd";
+const { Option } = Select;
 
+function GestorVoluntarios() {
+  const [rol, setRol] = useState(0);
+  const [users, setUsers] = useState([]);
+  const user = useSelector((state) => state.user);
+  const roles = useSelector((state) => state.roles.filter((rol) => rol.id > 2));
+  const history = useHistory();
 
+  useEffect(() => {
+    axios
+      .get(`/api/users/sede/${user.sedeId}`)
+      .then((res) => res.data)
+      .then((users) =>
+        setUsers(users.filter((userFilter) => userFilter.id !== user.id))
+      );
+  }, [user]);
 
-function GestorVoluntarios () {
+  const handleChange = (e) => {
+    setRol(e);
+  };
 
-    const [rol, setRol] = useState(0);
-    const [users, setUsers] = useState([])
-    const user = useSelector(state => state.user)
-    const roles = useSelector(state=>state.roles.filter((rol)=> rol.id > 2))
-    const history = useHistory();
+  const handleClick = (userId, rolId) => {
+    axios
+      .post("http://localhost:3001/api/roles/set/", { userId, rolId, user })
+      .then(() => {
+        axios
+          .get(`/api/users/sede/${user.sedeId}`)
+          .then((res) => res.data)
+          .then((users) =>
+            setUsers(users.filter((userFilter) => userFilter.id !== user.id))
+          );
+      });
+  };
 
-    useEffect(() => {
-        axios.get(`/api/users/sede/${user.sedeId}`)
-        .then(res => res.data)
-        .then(users => setUsers(users.filter(userFilter=>userFilter.id !== user.id)))
-    }, [user])
+  if (user.rolId && user.rolId !== 2) {
+    history.push("/unauthorized");
+    return (
+      <>
+        <h1>No autorizado</h1>
+      </>
+    );
+  }
 
-    const handleChange = (e) => {
-        setRol(e.target.value)
-    }
+  const dataSource = users.map((user) => {
+    return {
+      key: user.id,
+      nombre: user.full_name,
+      rol: user.rol?.tipo,
+    };
+  });
 
- 
-
-    const handleClick = (userId, rolId) => {
-        axios.post('http://localhost:3001/api/roles/set/', {userId, rolId, user}).then(()=>{
-            axios.get(`/api/users/sede/${user.sedeId}`)
-            .then(res => res.data)
-            .then(users => setUsers(users.filter(userFilter=>userFilter.id !== user.id)))
-        })
-    }
+  const columns = [
+    {
+      title: "Nombre",
+      dataIndex: "nombre",
+      key: "nombre",
+    },
+    {
+      title: "Rol",
+      dataIndex: "rol",
+      key: "rol",
+    },
+    {
+      title: "Seleccionar rol",
+      key: "seleccionar rol",
+      render: (text, record) => {
+        return (
+          <div>
+            <Select defaultValue="Seleccionar rol" style={{width:200}} onChange={handleChange}>
+              {roles &&
+                roles.map((rol) => {
+                  return <Option value={rol.id}> {rol.tipo} </Option>;
+                })}
+            </Select>
+            <Button
+              onClick={(e) => handleClick(record.key, parseInt(rol))}
+              type="button"
+            >
+              Asignar rol
+            </Button>
+          </div>
+        );
+      },
+    },
+  ];
 
   
-    if (user.rolId && user.rolId !== 2) {
-      history.push("/unauthorized");
-      return <><h1>No autorizado</h1></>;
-    }
-
-    return (
-        <div>
-            <h1>Administrar roles</h1>
-            {console.log('USERS', users)}
-            {!users.length && <h1>No hay voluntarios/gestores en esta sede</h1>}
-            {users && users.map(user => {
-                return (
-                    <div key={user.id}>
-                        <h2>Nombre: {user.full_name} Rol: {user.rol && user.rol.tipo}</h2>
-                        <label htmlFor='rol'>Seleccionar rol</label>
-                        <select onChange={handleChange}>
-                            <option>Seleccionar rol</option>
-                            {roles && roles.map(rol=>{
-                                return(
-                                <option value={rol.id}> {rol.tipo} </option>
-                                )
-                            })}
-                        </select>
-                        <button onClick={(e) => handleClick(user.id, parseInt(rol))} type='button'>Asignar rol</button>
-                    </div>
-                )
-            })}
-        </div>
-    )
+  return (
+    <div>
+      <h1>Administrar roles</h1>
+      {!users.length && <h1>No hay voluntarios/gestores en esta sede</h1>}
+      <Table dataSource={dataSource} columns={columns} pagination={false} />
+    </div>
+  );
 }
 
-export default GestorVoluntarios
+export default GestorVoluntarios;
