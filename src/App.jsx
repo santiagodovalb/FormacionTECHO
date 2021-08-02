9ccf019087fb74974489fe9ebe1feb147c6837b4
santikundera <santikundera@gmail.com>
2021-08-02 15:25:52 -0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import Login from "./containers/Login/Login";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setUser } from "./redux/user";
 import { Route, Switch, useHistory } from "react-router-dom";
@@ -14,31 +14,41 @@ import { getBloques } from "../src/redux/bloques";
 import { setUsers } from "./redux/users";
 import "./App.css";
 import Routes from "./Routes";
+import Loader from "react-loader-spinner";
 
 function App() {
+  const [loading, setLoading] = useState(true)
   const dispatch = useDispatch();
   const location = useLocation();
   const history = useHistory()
 
   useEffect(() => {
-    dispatch(setUsers())
-    dispatch(getBloques());
-    dispatch(getSedes());
-    dispatch(getRoles());
-
-    axios
-      .get("/api/users/me")
+      dispatch(setUsers())
+      .then(() => dispatch(getBloques()))
+      .then(() => dispatch(getSedes()))
+      .then(() => dispatch(getRoles()))
+      .then(() => dispatch(getBloques()))
+      .then(() => axios.get("/api/users/me"))
       .then((res) => res.data)
       .then((user) => axios.get(`/api/users/${user.id}`))
       .then((res) => res.data)
       .then((user) => dispatch(setUser(user)))
+      .then(() => setLoading(false))
       .catch(err => {
+        setLoading(false)
         if (!location.pathname.includes('login')) history.push('/login')
       })
-  }, [dispatch, history, location.pathname]);
+  }, [dispatch, history, location.pathname, loading]);
 
   return (
     <div className="App">
+      {console.log(loading)}
+      <Loader
+        id='loader'
+        type="Puff"
+        color="#00BFFF"
+        visible={loading}
+      />
       {!location.pathname.includes("login") && <Sidebar />}
       {!location.pathname.includes("login") && <Navbar />}
       <Switch>
