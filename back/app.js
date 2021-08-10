8241a01abd280c0c2571e03c373edf28765c72a7
santikundera <santikundera@gmail.com>
2021-08-10 10:29:27 -0300

    lpm

--- back/app.js
+++ back/app.js
@@ -14,7 +14,7 @@ app.use('/api', require('./routes'))
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("build"));
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+      res.sendFile(path.resolve(__dirname,  "../build", "index.html"));
     });
   }
 
