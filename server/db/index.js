28b88785b037eb7e433dba8daf9c9b679716e074
santikundera <santikundera@gmail.com>
2021-08-12 18:01:33 -0300

    db

--- server/db/index.js
+++ server/db/index.js
@@ -10,7 +10,6 @@ const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_PORT } = process.env;
 
 const client = new Sequelize(DB_DATABASE,DB_USERNAME, DB_PASSWORD, {
    host: DB_HOST,
-   port: DB_PORT,
    dialect: 'mysql',
   })
 
